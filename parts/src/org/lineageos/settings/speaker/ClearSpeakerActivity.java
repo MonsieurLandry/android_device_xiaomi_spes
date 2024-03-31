41ae43b9bf1511da40da49bc4d477ec932c0420f
MonsieurLandry <61120056+MonsieurLandry@users.noreply.github.com>
2024-04-01 00:21:17 +0200

    revert: spes: parts: fix: import org.lineageos.settings.R

--- parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -22,7 +22,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import org.lineageos.settings.R;
+import com.android.settingslib.widget.R;
 
 public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
 
