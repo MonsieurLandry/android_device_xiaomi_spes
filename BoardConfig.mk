2d935e5ce726bc572f9bc73bfcc28bd078edd03c
Michael Bestas <mkbestas@lineageos.org>
2024-06-29 11:41:56 +0200

    spes: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,6 +112,7 @@ TARGET_OTA_ASSERT_DEVICE := spes,spesn
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
